510e69dc85f33010fd673290059659a348ba5734
Colby Melvin <colby.g.melvin@gmail.com>
2024-08-11 19:33:38 -0400

    sudo rm

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,7 @@ sudo /usr/bin/podman-compose down
 
 # Stop the homelab-monitoring service
 sudo systemctl stop homelab-monitoring
-sudo /etc/systemd/system/homelab-monitoring.service
+sudo rm /etc/systemd/system/homelab-monitoring.service
 
 # Reload unit files and rebuild dependency trees
 sudo systemctl daemon-reload
